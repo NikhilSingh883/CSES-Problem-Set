7ea9aca0d3b97cb90a1512d970bb2139c39ad776
NikhilSingh883 <56299991+NikhilSingh883@users.noreply.github.com>
2021-08-30 19:08:57 +0530

    Introductory Problems: Two Knights

--- Introductory Problems/Two Knights.cpp
+++ Introductory Problems/Two Knights.cpp
@@ -22,10 +22,11 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
     ll n;
     cin>>n;
-    repf(i,1,n)
-    cout<<((i*i*i*i)-(i*i))/2 - 4*(i-1)*(i-2)<<"\n";
     
-        
-    }
+    repf(i,1,n)
+        cout<<((i*i*i*i)-(i*i))/2 - 4*(i-1)*(i-2)<<"\n";
+            
+}
